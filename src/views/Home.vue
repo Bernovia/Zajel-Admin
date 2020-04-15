430f3403d849c1deb78f677f47aa4cdd8f9024ec
Mohamad Alhaj Rahmoun <mrahmoun93@gmail.com>
2020-04-16 03:28:10 +0800

    dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,14 +23,10 @@
                             <!-- small box -->
                             <div class="small-box bg-info">
                                 <div class="inner">
-                                    <h3>150</h3>
+                                    <h3>{{usersCount}}</h3>
 
                                     <p>Users</p>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-bag"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <!-- ./col -->
@@ -38,14 +34,10 @@
                             <!-- small box -->
                             <div class="small-box bg-success">
                                 <div class="inner">
-                                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                                    <h3>{{booksCount}}</h3>
 
                                     <p>Books</p>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <!-- ./col -->
@@ -53,14 +45,10 @@
                             <!-- small box -->
                             <div class="small-box bg-warning">
                                 <div class="inner">
-                                    <h3>44</h3>
+                                    <h3>{{requestsCount}}</h3>
 
                                     <p>Requests</p>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-person-add"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <!-- ./col -->
@@ -68,14 +56,10 @@
                             <!-- small box -->
                             <div class="small-box bg-danger">
                                 <div class="inner">
-                                    <h3>65</h3>
+                                    <h3>{{conversationsCount}}</h3>
 
                                     <p>Conversations</p>
                                 </div>
-                                <div class="icon">
-                                    <i class="ion ion-pie-graph"></i>
-                                </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
                         <!-- ./col -->
@@ -97,6 +81,28 @@
 
 <script>
 
-export default {
-}
+    export default {
+        data (){
+            return {
+                usersCount: 0,
+                booksCount: 0,
+                requestsCount: 0,
+                conversationsCount: 0
+            }
+        },
+        created() {
+            this.fetchData();
+        },
+        methods: {
+            fetchData(){
+                this.$http.get('admin/metadata')
+                    .then(response => {
+                        this.usersCount = response.body.metadata.users_count
+                        this.booksCount = response.body.metadata.books_count
+                        this.requestsCount = response.body.metadata.requests_count
+                        this.conversationsCount = response.body.metadata.conversations_count
+                    })
+            }
+        }
+    }
 </script>
